38d6cb9bfee22fdf1b89e1abc96afe888d826f54
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-05-26 22:39:06 +0530

    delete: nectjs files

--- src/app/api/dashboard/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-type ResponseData = { message: string };
-
-function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-  res.status(200).json({ message: "Hello from Next.js!" });
-}
-
-export default handler;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-import { api } from "@/trpc/server";
-
-export default async function Home() {
-  return <div className="w-full bg-white p-2">Homepage</div>;
-}
