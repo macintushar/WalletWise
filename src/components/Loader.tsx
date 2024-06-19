0fcf1a7bcbde15d866bd18a01516846a27578790
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-06-19 19:12:31 +0530

    feat: added loader and sheet components

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h2 className="text-xl font-medium">Loading...</h2>
+    </div>
+  );
+}
